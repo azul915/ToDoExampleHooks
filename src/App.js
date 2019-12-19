818f449d7af73995057390da1e29358b2902dcb3
KoseiAoyagi <45550286+azul915@users.noreply.github.com>
2019-12-19 21:23:57 +0900

    Add Form to create new list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React, { useState } from "react";
 
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.priventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
 const App = () => {
   const [todos, setTodos] = useState([
-    { text: "React Hooksについて学ぶ" },
-    { text: "GoModulesとパッケージ構成の整理" },
-    { text: "TypeScriptの言語仕様" }
+    {
+      text: "React Hooksについて学ぶ",
+      isCompleted: false
+    },
+    {
+      text: "GoModulesとパッケージ構成の整理",
+      isCompleted: false
+    },
+    {
+      text: "TypeScriptの言語仕様",
+      isCompleted: false
+    }
   ]);
 
-  return <div />;
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
